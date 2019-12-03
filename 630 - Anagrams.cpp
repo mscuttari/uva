ed3623fc4a5e4610bfc0a57e04fb4b64089de262
Michele Scuttari <mscuttari@users.noreply.github.com>
2019-12-03 22:34:39 +0100

    630: solution was wrong but still accepted

--- 630 - Anagrams.cpp
+++ 630 - Anagrams.cpp
@@ -1,15 +1,11 @@
-#include <iostream>
-#include <map>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-static int primes[26] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101};
-
 int getKey(const string& str);
+bool areAnagrams(const string& str1, const string& str2);
 
 int main() {
-
     int t;
     cin >> t;
 
@@ -48,9 +44,15 @@ int main() {
 
             if (it != anagrams.end()){
                 vector<string> &v = it->second;
+                int count = 0;
 
                 for (int i = 1; i <= v.size(); i++) {
-                    cout << "  " << i << ") " << v[i - 1] << endl;
+                    if (areAnagrams(str, v[i - 1]))
+                        cout << "  " << ++count << ") " << v[i - 1] << endl;
+                }
+
+                if (count == 0) {
+                    cout << "No anagrams for: " << str << endl;
                 }
 
             } else {
@@ -73,8 +75,30 @@ int getKey(const string& str) {
     int hash = 0;
 
     for (char const &c : str) {
-        hash += primes[c - 'a'];
+        hash += c;
     }
 
     return hash;
+}
+
+bool areAnagrams(const string& str1, const string& str2) {
+    int n1 = str1.length();
+    int n2 = str2.length();
+
+    if (n1 != n2) {
+        return false;
+    }
+
+    string x(str1);
+    string y(str2);
+
+    sort(x.begin(), x.end());
+    sort(y.begin(), y.end());
+
+    for (int i = 0; i < n1; i++) {
+        if (x[i] != y[i])
+            return false;
+    }
+
+    return true;
 }
